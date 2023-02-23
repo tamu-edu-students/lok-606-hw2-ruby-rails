135aecc58bab302687fc372502213773897b46ed
Lok Pulivarthi <Loksp27@gmail.com>
2023-02-22 21:53:52 -0600

    Files updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+  
+# end
+
+
+
+Rails.application.routes.draw do
+  resources :movies
+  root :to => redirect('/movies')
 end
